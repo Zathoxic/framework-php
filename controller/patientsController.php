5b74ccf6aa69266962f2d0d665922cdc26916642
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-04-05 11:48:47 +0200

    All bugs are fixed

--- controller/patientsController.php
+++ controller/patientsController.php
@@ -1,11 +1,13 @@
 <?php
-
+//Include all required models
 require(ROOT . "model/patientModel.php");
 require(ROOT . "model/clientModel.php");
 require(ROOT . "model/speciesModel.php");
 
-function index(){
 
+//Show indexes
+function index(){
+    //Check if the orders are other then default
     if(isset($_GET['order'])){
         $order = $_GET['order'];
     } else{
@@ -16,26 +18,28 @@ function index(){
     } else{
         $sort = 'DESC';
     }
-
+    
+    //This line checks the state of the sorting and changes it depending on the previous state
     $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
-
+    
+    //Forward and display the functions and variabels to the index
 	render("patients/index", array(
 		'patients' => getAllPatients($order, $sort),
         'order' => $order,
-        'sortAllPatients' => sortAllPatients($order, $sort),
         'sort' => $sort
 	));
 }
 
-function create(){
 
-	//formulier tonen
+//Shows create page
+function create(){
+	//Show form
 	render("patients/create", array(
-            'patients' => getAllPatients(),
             'clients' => getAllClients(),
             'species' => getAllSpecies()
     ));
 }
+
 
 function createSave(){
 
@@ -51,8 +55,8 @@ function edit($id){
 	$patient = getPatient($id);
 
     getPatient($id);
-
-
+    
+    
 	if(isset($_POST['submit'])){
         $id = $_POST['id'];
         $name = $_POST['name'];
@@ -60,21 +64,20 @@ function edit($id){
         $status = $_POST['status'];
         $owner = $_POST['owner'];
         $gender = $_POST['gender'];
-
+        
         editPatient($id, $name, $species, $status, $owner, $gender);
     }
 
 	render("patients/edit", array(
             'patient' => getPatient($id),
-            'patients' => getAllPatients($order, $sort),
             'clients' => getAllClients(),
             'species' => getAllSpecies()
     ));
 }
 
 function editSave(){
-
-}
+    
+} 
 
 function delete($id){
 	if (isset($id)) {
--- model/patientModel.php
+++ model/patientModel.php
@@ -1,87 +1,87 @@
 <?php
 
+
+// This function fetches the patient you want to edit/delete and requires the ID of the patient
 function getPatient($id) {
+    //Opening database connection and storing it in $db
 	$db = openDatabaseConnection();
 
+    //Creating a query
 	$sql = "SELECT * FROM patients WHERE id=:id";
 	$query = $db->prepare($sql);
+    
+    //Executing the query
 	$query->execute(array(
         ':id' => $id
     ));
 
+    //Closing database connection
 	$db = null;
 
+    //Show the results from the query
 	return $query->fetchAll();
 
 }
 
-function sortAllPatients($order, $sort){
+//This function fetches all patients to display on the index page and also makes us able to sort them
+function getAllPatients($order, $sort) {
+    //Opening database connection and storing it in $db
 	$db = openDatabaseConnection();
-
+    
+    //Creating a query
 	$sql = "SELECT * FROM patients";
-
+    
+    //Checking if the requered variables are set for sorting
     if(isset($order)&&isset($sort)){
         $sql .= ' ORDER BY ' . $order .  ' ' . $sort;
     }
-
-
+    
+    
 	$query = $db->prepare($sql);
+    
+    //Executing the query
 	$query->execute();
 
+    //Closing database connection
 	$db = null;
 
-	return $query->fetchAll();
-}
-
-function getAllPatients() {
-	$db = openDatabaseConnection();
-
-	$sql = "SELECT * FROM patients";
-
-
-	$query = $db->prepare($sql);
-	$query->execute();
-
-	$db = null;
-
+    //Show the results from the query
 	return $query->fetchAll();
 
 }
 
-function orderPatients(){
-    // Create database connection
-    $db = openDatabaseConnection();
-}
-
-
-
+//This function lets us edit the patient and requires us to enter the name, species, status, owner and gender. The ID comes with the form when we click edit
 function editPatient($id, $name, $species, $status, $owner, $gender) {
     // Create database connection
     $db = openDatabaseConnection();
-
+    
     // Get all clients
     $ownerQuery = ("SELECT * FROM clients");
     $query = $db->prepare($ownerQuery);
+    
+    //Execute the query
     $query->execute();
-
-
-
+    
+    
+    
     // Check if all inputs from the form have been entered
-    if($name && $species && $status && $owner && $gender){
-
+    if($name && $species && $status && $owner && $gender){  
+        
         // Run a query to check if the patient exists
         $exists = ("SELECT * FROM patients WHERE id=':id'");
         $query = $db->prepare($exists);
         $query->execute(array(
 		':id' => $id
 		)) or die ("The query could not be completed");
-
+        
         $count = $query->rowCount();
         if($count != 1){
-
+            
             // If there IS a patient, update it
-            $sql = "UPDATE patients SET name=:newname, species=:newspecies, status=:newstatus, owner=:newowner, gender:newgender WHERE id=:existingid ";
+            $sql = "UPDATE patients SET name = :newname, species = :newspecies, status = :newstatus, owner = :newowner, gender = :newgender WHERE id=:existingid ";
             $query = $db->prepare($sql);
+            
+            //execute the query
             $query->execute(array(
                 ':newname' => $name,
                 ':newspecies' => $species,
@@ -93,27 +93,37 @@ function editPatient($id, $name, $species, $status, $owner, $gender) {
         $db = null;
         echo "<h1 class='Success'>Successfully updated patient</h1>";
         } else echo "<h1 class='Fail'>That patient does not exist! Please try again:</h1>"; // If the patient DOESNT exist, echo: That patient does not exist! Please try again:
-
-    } else echo "<h1 class='Fail'>Whoops! You have not filled in all fields. Please try again:</h1>";
+        
+    } else echo "<h1 class='Fail'>Whoops! You have not filled in all fields. Please try again:</h1>";//If you haven't filled in all fields, the page will return this error
 }
 
+
+//This function deletes the selected patient
 function deletePatient($id) {
+    //Open databaseconnection and store it in $db
 	$db = openDatabaseConnection();
 
 	$sql = "DELETE FROM patients WHERE id=:id ";
 	$query = $db->prepare($sql);
+    //Execute query
 	$query->execute(array(
 		':id' => $id
 		));
 
+    //Close database connection
 	$db = null;
 }
 
+//This function lets us create a patient. It also requires us to enter a name, species, status, owner and gender. Notice how it doesnt need the ID since it automatically gives it in the database
 function createPatient($name, $species, $status, $owner, $gender) {
+    //Open database connection and save it in $db
 	$db = openDatabaseConnection();
 
+    //Create query
 	$sql = "INSERT INTO patients(name, species, status, owner, gender) VALUES (:name, :species, :status, :owner, :gender)";
 	$query = $db->prepare($sql);
+    
+    //Execute query
 	$query->execute(array(
 		':name' => $name,
 		':species' => $species,
@@ -122,5 +132,6 @@ function createPatient($name, $species, $status, $owner, $gender) {
         ':gender' => $gender
 		));
 
+    //Close database connection
 	$db = null;
 }
